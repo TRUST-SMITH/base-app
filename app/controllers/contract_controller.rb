2bc84e01c0cff280f234e96537974ce8a2af516a
渡辺琢真 <watanabetakushin@watanabetakushinnoMacBook-Air-2.local>
2019-04-09 19:44:22 +0900

    first

---
+++ app/controllers/contract_controller.rb
@@ -0,0 +1,22 @@
+class ContractController < ApplicationController
+  def choice
+  end
+
+  def data
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def confirm
+  end
+
+  def sending
+  end
+
+  def deleate
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'contract/choice'
+  get 'contract/data'
+  get 'contract/create'
+  get 'contract/edit'
+  get 'contract/confirm'
+  get 'contract/send'
+  get 'contract/deleate'
+  get 'home/top'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
